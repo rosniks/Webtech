927eaeee963c41c89d2a6e64534d0ce60f236c71
LaraBiewers <118667773+LaraBiewers@users.noreply.github.com>
2022-12-13 14:15:22 +0100

    on-click-demo

--- src/js/table-onClick.js
+++ src/js/table-onClick.js
@@ -1,1 +1,15 @@
 /* Refernez: https://robertcooper.me/post/table-row-links */
+
+function on_click() {
+
+    for (let i = 1; i <= 6; i++) {
+
+        const row = document.getElementById(`row-${i}`);
+
+        row.addEventListener("click", () => {
+            window.location.href = `/row-${i}`;
+        });
+
+    }
+
+}
