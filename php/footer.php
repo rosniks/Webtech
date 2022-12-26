3c640c8fc86d00ddb67487346f3dab95ef9b125b
Niklas <nkrs2321@hocschule-trier.de>
2022-12-26 20:40:15 +0100

    fix

--- php/footer.php
+++ php/footer.php
@@ -1,15 +1,15 @@
 <footer>
-            <div class="col1">
-                <h3>Kontakt</h3>
-                <p>
-                    Gemeindezentrum Trier-West<br>
-                    In dieser Straße 7<br>
-                    54293 Trier<br>
-                    Tel.: 06511 / 123456789<br>
-                </p>
-            </div>
-            <div class="col2">
-                <a href="impressum.html">Impressum</a>
-                <a href="datenschutzrichtlinien.html">Datenschutzrichtlinien</a>
-            </div>
-        </footer>
+    <div class="col1">
+        <h3>Kontakt</h3>
+        <p>
+            Gemeindezentrum Trier-West<br>
+            In dieser Straße 7<br>
+            54293 Trier<br>
+            Tel.: 06511 / 123456789<br>
+        </p>
+    </div>
+    <div class="col2">
+        <a href="impressum.html">Impressum</a>
+        <a href="datenschutzrichtlinien.html">Datenschutzrichtlinien</a>
+    </div>
+</footer>
