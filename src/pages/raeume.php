7e9ffa57293e6f359f6c46232d19ee01ea71dcdb
Niklas <nkrs2321@hocschule-trier.de>
2022-12-30 16:20:48 +0100

    Räume überarbeitet

--- src/pages/raeume.php
+++ src/pages/raeume.php
@@ -20,18 +20,28 @@
         <div class="raumplan-container">
             <div class="raumplanEl">
                 <h1>Erdgeschoss</h1>
-                <img src="../../img/RaumplanEG.png" alt="Raumplan des Erdgeschosses">
+                <img src="../../img/RaumplanEG.png" alt="Raumplan des Erdgeschosses" usemap="#erdgeschoss">
+                <map name="erdgeschoss">
+                    <area alt="Saal 1" title="Saal1" href="#saal1" coords="12,260,250,415" shape="rect">
+                    <area alt="Gemeinschaftsraum" title="Gemeinschaftsraum" href="#gmraum" coords="330,250,570,415" shape="rect">
+                    <area alt="Saal 2" title="Saal2" href="#saal2" coords="12,80,250,255" shape="rect">
+                </map>
             </div>
-            <div class="raumpplanEl">
+            <div class="raumplanEl">
                 <h1>Erster Stock</h1>
-                <img src="../../img/RaumplanOG.png" alt="Raumplan des Ersten Stocks">
+                <img src="../../img/RaumplanOG.png" alt="Raumplan des Ersten Stocks" usemap="#ersterstock">
+                <map name="ersterstock">
+                    <area alt="Z-Räume" title="Z-Räume" href="#z" coords="12,320,320,415" shape="rect">
+                    <area alt="Saal 3" title="Saal 3" href="#saal3" coords="330,250,570,415" shape="rect">
+                    <area alt="Küche" title="Küche" href="#kueche" coords="12,80,250,220" shape="rect">
+                </map>
             </div>
         </div>
 
         <hr>
 
         <div class="container-raumbeschreibung">
-            <h1>Gemeinschaftsraum</h1>
+            <h1 id="gmraum">Gemeinschaftsraum</h1>
             <img class="raumbild" src="../../img/Gemeinschaftsraum.jpg" alt="Gemeinschaftsraum">
             <p>
                 Ein Ort um sich mit Freunden und Bekannten zu Treffen und Spaß zu haben. Unser Gemeinschaftsraum bietet für jeden etwas. Seien es die Bücher oder Brettspiele, der Kickertisch, oder einfach die Menschen.
@@ -43,8 +53,8 @@
         <hr>
 
         <div class="container-raumbeschreibung">
-            <h1>Saal 1</h1>
-
+            <h1 id="saal1">Saal 1</h1>
+            <img class="raumbild" src="../../img/saal1.jpg" alt="Saal 1">
 
             <p>
                 Der kleinste unserer Sääle, aber er hat viel zu bieten. Im Saal 1 hat man alles was man für eine gute Veranstaltung braucht. Tische und Stühle stehen frei zur Verfügung.
@@ -56,8 +66,8 @@
         <hr>
 
         <div class="container-raumbeschreibung">
-            <h1>Saal 2</h1>
-
+            <h1  id="saal2">Saal 2</h1>
+            <img class="raumbild" src="../../img/saal2.jpg" alt="Saal 2">
             <p>
                 Ein großer Raum für große Veranstaltungen. Sei es die Silvesterparty oder das Sommerfest, hier finden die Hauptveranstaltungen statt. Mit einer Kapazität von über 100 Personen kann man hier richtig groß Feiern.
 
@@ -67,8 +77,8 @@
         <hr>
 
         <div class="container-raumbeschreibung">
-            <h1>Saal 3</h1>
-
+            <h1 id="saal3">Saal 3</h1>
+            <img class="raumbild" src="../../img/saal3.jpg" alt="Saal 3">
             <p>
                 <strong>"The Show must go on!"</strong> So zumindest das Motto dieses Saals. Mit der großen Tribüne, den vielen Sitzplätzen, der Leinwand und der Lichtershow ist dieser Raum perfekt für alles das was beeindrucken soll.
                 Bands finden hier den besten Ort um Konzerte zu halten. Regelmäßig finden Filmabende statt, Künstler haben die Chance sich zu präsentieren und mehr.
@@ -79,8 +89,8 @@
         <hr>
 
         <div class="container-raumbeschreibung">
-            <h1>Z1, Z2, Z3</h1>
-
+            <h1 id="z">Z1, Z2, Z3</h1>
+            <img class="raumbild" src="../../img/z.jpg" alt="Z Räume">
             <p>
                 Sie möchten einen schönen Abend unter Freunden machen und wissen nicht wohin? Die Räume Z1-3 bieten sich hier perfekt an. Reservieren Sie für sich einen dieser Räume und haben Sie Spaß.
                 In den Räumen befinden sich je zwei Tische und acht bequemen Stühle, die Sie so verschieben können wie Sie möchten. Seien es Spielabende, kleine Tuniere oder auch einfach ein netter Abend unter Freuden,
@@ -91,8 +101,8 @@
         <hr>
 
         <div class="container-raumbeschreibung">
-            <h1>Küche</h1>
-
+            <h1 id="kueche">Küche</h1>
+            <img class="raumbild" src="../../img/kueche.jpg" alt="Küche">
             <p>
                 Die modern ausgestattete Küche ist perfekt fürs Kochen. Hier werden die Kuchen für die Sonntagskaffees gebacken, die Snacks für die Partys vorbereitet und mehr.
                 Die Küche kann für eine Veranstaltung dazu gebucht werden, oder für zum Beispiel Kochkurse verwendet werden. Falls Sie die Küche extra dazubuchen möchten, geben Sie dies bitte im Eingabefeld des Formulars an. 
