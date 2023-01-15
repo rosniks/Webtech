260dded7d7b86d4da43cf012f00f2e3a42b677e1
Niklas <nkrs2321@hocschule-trier.de>
2023-01-15 12:09:52 +0100

    Ordner Webtech -> webseite

--- src/php/footer.php
+++ src/php/footer.php
@@ -7,7 +7,7 @@
             54294 Trier<br>
             Tel.: 06511 / 123456789<br><br>
             <?php 
-            if ($_SERVER["PHP_SELF"] == "/Webtech/index.php" || $_SERVER["PHP_SELF"] == "/Webtech"){
+            if ($_SERVER["PHP_SELF"] == "/webseite/index.php" || $_SERVER["PHP_SELF"] == "/webseite"){
                 echo ('
                 <a href="#" class="icon"><img src="img/Facebook.png" alt="Facebook"></a>
                 <a href="#" class="icon"><img src="img/Instagram.png" alt="Instagram"></a>
--- src/php/nav.php
+++ src/php/nav.php
@@ -13,32 +13,32 @@
             <ul>
                 <li>
                     <a href="veranstaltungen.php"
-                        class="<?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/veranstaltungen.php' || $_SERVER['PHP_SELF'] == '/Webtech/src/pages/kurse.php' ? 'aktuell' : ''); ?>">Veranstaltungen</a>
+                        class="<?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/veranstaltungen.php' || $_SERVER['PHP_SELF'] == '/webseite/src/pages/kurse.php' ? 'aktuell' : ''); ?>">Veranstaltungen</a>
                 </li>
                 <li>
                     <a href="raeume.php"
-                        class="<?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/raeume.php' ? 'aktuell' : ''); ?>">Räume</a>
+                        class="<?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/raeume.php' ? 'aktuell' : ''); ?>">Räume</a>
                 </li>
                 <li>
                     <a href="uberuns.php"
-                        class="<?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/uberuns.php' ? 'aktuell' : ''); ?>">Über
+                        class="<?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/uberuns.php' ? 'aktuell' : ''); ?>">Über
                         Uns</a>
                 </li>
                 <li>
                     <a href="galerie.php"
-                        class="<?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/galerie.php' ? 'aktuell' : ''); ?>">Galerie</a>
+                        class="<?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/galerie.php' ? 'aktuell' : ''); ?>">Galerie</a>
                 </li>
                 <li>
                     <a href="kontakt.php"
-                        class="<?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/kontakt.php' ? 'aktuell' : ''); ?>">Kontakt</a>
+                        class="<?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/kontakt.php' ? 'aktuell' : ''); ?>">Kontakt</a>
                 </li>
             </ul>
 
-            <?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/veranstaltungen.php' ? '<ul>
+            <?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/veranstaltungen.php' ? '<ul>
                         <li class="aktuell"><a href="veranstaltungen.php">aktuelle Veranstaltungen</a></li>
                         <li><a href="kurse.php">aktuelle Kurse</a></li>
             </ul>' : ''); ?>
-            <?php echo ($_SERVER['PHP_SELF'] == '/Webtech/src/pages/kurse.php' ? '<ul>
+            <?php echo ($_SERVER['PHP_SELF'] == '/webseite/src/pages/kurse.php' ? '<ul>
                         <li><a href="veranstaltungen.php">aktuelle Veranstaltungen</a></li>
                         <li class="aktuell"><a href="kurse.php">aktuelle Kurse</a></li>
             </ul>' : ''); ?>
